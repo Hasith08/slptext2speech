d892562fbb522503d4ad14b276ce90ffe76899b5
Hasith08 <mohamedhasith16@gmail.com>
2024-04-03 12:20:42 +0530

    first comment

--- app.py
+++ app.py
@@ -6,18 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', audio=False)
 
 @app.route('/convert', methods=['POST'])
 def convert():
     if request.method == 'POST':
         text = request.form['text']
         tts = gTTS(text=text, lang='en')
-        tts.save("output.mp3")
-        os.system("start output.mp3")  # On Windows
-        # For Linux/Mac use:
-        # os.system("xdg-open output.mp3")
-        return redirect(url_for('index'))
+        tts.save("static/output.mp3")
+        return render_template('index.html', audio=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
